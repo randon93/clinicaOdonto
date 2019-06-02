d56bfc52761b7a275249221cbea4199e665d3dd9
randon93 <32597818+randon93@users.noreply.github.com>
2019-06-02 14:18:36 -0500

    ACTUALIZAR RUTAS

--- VISTA/plantilla.php
+++ VISTA/plantilla.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../font/fontawesome.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>PUBLIC/font/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>PUBLIC/css/style.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
