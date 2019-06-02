36448ca4980fd4430e7912d7a32ffbdddba35ae1
randon93 <32597818+randon93@users.noreply.github.com>
2019-06-02 12:20:05 -0500

    BASE DATOS

--- adontologia.sql
+++ adontologia.sql
@@ -1,173 +1,124 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2019 a las 22:50:41
--- Versión del servidor: 10.1.36-MariaDB
--- Versión de PHP: 7.2.11
+CREATE TABLE persona (
+  cedula varchar(10) PRIMARY KEY,
+  nonbre varchar(50),
+  correo varhar(50),
+  telefono varchar(50)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE odontologo (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  cedula varchar(10),
+  INDEX (cedula),
+  FOREIGN KEY (cedula) REFERENCES persona(cedula)
+);
 
+CREATE TABLE paciente (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  cedula varchar(10),
+  INDEX (cedula),
+  FOREIGN KEY (cedula) REFERENCES persona(cedula)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE auxiliar (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  cedula varchar(10),
+  INDEX (cedula),
+  FOREIGN KEY (cedula) REFERENCES persona(cedula)
+);
 
---
--- Base de datos: `adontologia`
---
+CREATE TABLE administrador (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  cedula varchar(10),
+  INDEX (cedula),
+  FOREIGN KEY (cedula) REFERENCES persona(cedula)
+);
 
--- --------------------------------------------------------
+CREATE TABLE colsultorio (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  nombre varchar(50),
+  direccion varchar(50),
+  correo varhar(30),
+  telefono varhar(10)
+);
 
---
--- Estructura de tabla para la tabla `administrador`
---
+CREATE TABLE consultorio_persona (
+  id_consiltorio INT NOT NULL,
+  cedula_p varchar(10),
+  INDEX (id_consiltorio),
+  INDEX (cedula_p),
+  FOREIGN KEY (id_consiltorio) REFERENCES cosnultorio(id),
+  FOREIGN KEY (cedula_p) REFERENCES persona(cedula)
+);
 
-CREATE TABLE `administrador` (
-  `nombre` varchar(59) DEFAULT NULL,
-  `cedula` varchar(10) NOT NULL,
-  `correo` varchar(50) DEFAULT NULL,
-  `telefono` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE consultorio_auxialiar (
+  cedula_a varhar(50),
+  id_consiltorio INT NOT NULL,
+  CONSTRAINT consul_auxi PRIMARY KEY (cedula_a, id_consiltorio)
+);
 
--- --------------------------------------------------------
+CREATE TABLE especialidad (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  nombre varhar(50)
+);
 
---
--- Estructura de tabla para la tabla `adontologo`
---
+CREATE TABLE odontologo_especialidad (
+  id_especialidad INT NOT NULL,
+  cedula_o INT NOT NULL,
+  CONSTRAINT odont_espec PRIMARY KEY (id_especialidad, cedula_o),
+  INDEX (id_especialidad),
+  INDEX (cedula_o),
+  FOREIGN KEY (id_especialidad) REFERENCES especialidad(id),
+  FOREIGN KEY (cedula_o) REFERENCES odontologo(id),
+);
 
-CREATE TABLE `adontologo` (
-  `nombre` varchar(59) DEFAULT NULL,
-  `cedula` varchar(10) NOT NULL,
-  `correo` varchar(50) DEFAULT NULL,
-  `telefono` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE cita (
+  numero_cita INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  fecha_solicitud date NOT NULL,
+  cedula_p INT NOT NULL,
+  id_consultorio INT NOT NULL,
+  fecha_asignada date NOT NULL,
+  cedula_o INT NOT NULL,
+  INDEX (cedula_p),
+  INDEX (cedula_o),
+  INDEX (id_consultorio)
+  FOREIGN KEY (id_consultorio) REFERENCES consultorio(id),
+  FOREIGN KEY (cedula_o) REFERENCES odontologo(id),
+  FOREIGN KEY (cedula_p) REFERENCES paciente(id),
+);
 
--- --------------------------------------------------------
+CREATE TABLE atencion_cita (
+  numero_cita INT NOT NULL PRIMARY KEY,
+  fecha_asignada date NOT NULL
+);
 
---
--- Estructura de tabla para la tabla `cita`
---
+CREATE TABLE procedimiento_paciente (
+  id_procedimiento INT NOT NULL,
+  cecula_p INT not NULL,
+  INDEX (cedula_p),
+  INDEX (id_procedimiento),
+  CONSTRAINT proce_paci PRIMARY KEY (cedula_p, id_procedimiento),
+  FOREIGN KEY (cedula_p) REFERENCES paciente(id),
+  FOREIGN KEY (id_procedimiento) REFERENCES procedimiento(id),
+);
 
-CREATE TABLE `cita` (
-  `id` int(11) NOT NULL,
-  `cliente` varchar(10) DEFAULT NULL,
-  `odonto` varchar(10) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `id_servicio` varchar(2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE procedimiento (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT null,
+  nombre varhar(50)
+);
 
--- --------------------------------------------------------
+CREATE TABLE factura_paciente (
+  id_factura INT,
+  cedula_p INT,
+  INDEX (cedula_p),
+  INDEX (id_factura),
+  CONSTRAINT fact_paci PRIMARY KEY (cedula_p, id_factura),
+  FOREIGN KEY (id_factura) REFERENCES factura(id),
+  FOREIGN KEY (cedula_p) REFERENCES paciente(id),
+);
 
---
--- Estructura de tabla para la tabla `cliente`
---
-
-CREATE TABLE `cliente` (
-  `nombre` varchar(59) DEFAULT NULL,
-  `cedula` varchar(10) NOT NULL,
-  `correo` varchar(50) DEFAULT NULL,
-  `telefono` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `factura`
---
-
-CREATE TABLE `factura` (
-  `id` int(11) NOT NULL,
-  `cita` int(11) NOT NULL,
-  `total` int(11) DEFAULT NULL,
-  `admin` varchar(10) DEFAULT NULL,
-  `cliente` varchar(10) DEFAULT NULL,
-  `fecha` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `administrador`
---
-ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`cedula`),
-  ADD KEY `cedula` (`cedula`);
-
---
--- Indices de la tabla `adontologo`
---
-ALTER TABLE `adontologo`
-  ADD PRIMARY KEY (`cedula`),
-  ADD KEY `cedula` (`cedula`);
-
---
--- Indices de la tabla `cita`
---
-ALTER TABLE `cita`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cliente` (`cliente`),
-  ADD KEY `odonto` (`odonto`),
-  ADD KEY `id_servicio` (`id_servicio`);
-
---
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`cedula`);
-
---
--- Indices de la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cita` (`cita`),
-  ADD KEY `admin` (`admin`),
-  ADD KEY `cliente` (`cliente`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `cita`
---
-ALTER TABLE `cita`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `factura`
---
-ALTER TABLE `factura`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `cita`
---
-ALTER TABLE `cita`
-  ADD CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`odonto`) REFERENCES `adontologo` (`cedula`),
-  ADD CONSTRAINT `cita_ibfk_2` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`cedula`),
-  ADD CONSTRAINT `cita_ibfk_3` FOREIGN KEY (`id`) REFERENCES `factura` (`cita`);
-
---
--- Filtros para la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`admin`) REFERENCES `administrador` (`cedula`),
-  ADD CONSTRAINT `factura_ibfk_2` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`cedula`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE factura (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  fecha date,
+  valor INT,
+  pago varchar (20)
+);
